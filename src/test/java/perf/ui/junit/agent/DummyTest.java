93d2f562cb0694eedea203b974c2353c5c6d747b
Ivan Nahornyi <hupfyn@gmail.com>
2019-08-05 21:06:54 +0300

    Implement perfUi like test watcher

--- src/test/java/perf/ui/junit/agent/DummyTest.java
+++ src/test/java/perf/ui/junit/agent/DummyTest.java
@@ -3,34 +3,31 @@ package perf.ui.junit.agent;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import perf.ui.junit.agent.annotations.PerfUI;
 
 import java.util.Objects;
 
-@RunWith(PerfUITestRunner.class)
 public class DummyTest {
 
     private WebDriver driver;
     private WebDriverWait waiter;
 
-    public WebDriver getWebDriver() {
-        return driver;
-    }
+    @Rule
+    public PerfUIMetricGrabber perfUIMetricGrabber = new PerfUIMetricGrabber();
 
     @Before
     public void setUp() {
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver(new ChromeOptions().addArguments("--remote-debugging-port=9222"));
-        driver.manage().window().maximize();
+        driver = new ChromeDriver();
         waiter = new WebDriverWait(driver, 10);
+
     }
 
     @PerfUI
@@ -50,6 +47,7 @@ public class DummyTest {
 
     @After
     public void shutDown() {
+        perfUIMetricGrabber.runAudit(driver);
         if (!Objects.isNull(driver)) {
             driver.quit();
         }
